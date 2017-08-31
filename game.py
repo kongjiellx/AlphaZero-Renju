9a1cde56ab668e2626d9de378eb5015420fe959f
刘也宽 <liuyekuan@bytedance.com>
2017-08-31 20:49:39 +0800

    first

--- game.py
+++ game.py
@@ -4,29 +4,50 @@ import numpy as np
 
 
 def one_hot(x):
-    ret = np.zeros(25)
-    ret[x] = 1
-    return ret
+    ret = []
+    for n in x:
+        t = np.zeros(25)
+        t[n] = 1
+        ret.append(t)
+    return np.array(ret)
 
 
 class Game(object):
     def __init__(self):
-        self.board = Board()
+
         self.model = Model()
+        self.model.load()
 
     def play(self):
-        observation = self.board.board
-        done = False
-        for i in range(10):
+        ps = 0
+        for i in range(1000000):
+            if i % 10000 == 0:
+                self.model.save()
+                print(ps)
+                ps = 0
+
+            self.board = Board()
+            # print('*******************')
+            observation = self.board.board
+            done = False
             sum_reward = 0
+            predicts = []
+            points = []
+            X = []
             while(not done):
+                X.append(observation.flatten())
                 predict = self.model.forward(observation.flatten())
                 point = np.argmax(predict)
+                predicts.append(predict)
+                points.append(point)
                 done, reward, observation = self.board.step((point // 5, point % 5))
                 sum_reward += reward
-                print(observation)
-            grad = self.model.backward(sum_reward, one_hot(point), predict)
-            self.model.update(grad, 0.01)
+                # print(observation)
+                # print('______________')
+            grad = self.model.backward(sum_reward, X, one_hot(points), predicts)
+            self.model.update(grad, 0.0001)
+            ps += sum_reward
+        self.model.save()
 
 if __name__ == '__main__':
     g = Game()
--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import numpy as np
+import pickle
 
 
 class Model(object):
@@ -15,12 +16,23 @@ class Model(object):
     def forward(self, x):
         return self.softmax(np.dot(self.W, x) + self.b)
 
-    def backward(self, reward, y, y_):
-        return np.dot(reward.T, y - y_)
+    def backward(self, reward, X, y, y_):
+        return {'W': reward * np.dot(np.array(X).T, (y - y_)), 'b': reward* np.sum(y - y_)}
 
     def update(self, grad, lr):
         self.W += grad['W'] * lr
         self.b += grad['b'] * lr
 
+    def save(self, path='model.bin'):
+        fp = open(path, 'wb')
+        pickle.dump({'W': self.W, 'b': self.b}, fp)
+        fp.close()
+
+    def load(self, path='model.bin'):
+        fp = open(path, 'rb')
+        param = pickle.load(fp)
+        fp.close()
+        self.W = param['W']
+        self.b = param['b']
 
 
