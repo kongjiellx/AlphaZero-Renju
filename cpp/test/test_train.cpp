d4ced90623d0f317b827e07c48eb42be93579f8c
kongjiellx <623239411@qq.com>
2019-10-19 19:20:16 +0800

    training example

--- cpp/test/test_train.cpp
+++ cpp/test/test_train.cpp
@@ -42,7 +42,7 @@ int main() {
     auto y = Placeholder(scope, DT_FLOAT);
 
     auto w1 = Variable(scope, {3, 1}, DT_FLOAT);
-    auto assign_w1 = Assign(scope, w1, RandomNormal(scope, {3, 1}, DT_FLOAT));
+    auto assign_w1 = Assign(scope, w1, XavierInit(scope, 3, 1, 0));
 
     auto b1 = Variable(scope, {1, 1}, DT_FLOAT);
     auto assign_b1 = Assign(scope, b1, RandomNormal(scope, {1, 1}, DT_FLOAT));
@@ -53,17 +53,27 @@ int main() {
     std::vector<Output> grad_outputs;
     TF_CHECK_OK(AddSymbolicGradients(scope, {loss}, {w1, b1}, &grad_outputs));
     auto apply_w1 = ApplyGradientDescent(scope, w1, Cast(scope, 0.01,  DT_FLOAT), {grad_outputs[0]});
-    auto apply_b1 = ApplyGradientDescent(scope, b1, Cast(scope, 0.01,  DT_FLOAT), {grad_outputs[3]});
-
+    auto apply_b1 = ApplyGradientDescent(scope, b1, Cast(scope, 0.01,  DT_FLOAT), {grad_outputs[1]});
     ClientSession session(scope);
     std::vector<Tensor> outputs;
 
     TF_CHECK_OK(session.Run({assign_w1, assign_b1}, nullptr));
 
+    std::vector<float> dx({1, 2, 3, 4, 5, 6, 7, 8, 9});
+    std::vector<float> dy({-1, 0, 1});
 
-//    TF_CHECK_OK(session.Run({{x, x_data},
-//                             {y, y_data}}, {loss}, &outputs));
-//    std::cout << "Loss: " << outputs[0].scalar<float>() << std::endl;
+    Tensor x_data(DataTypeToEnum<float>::v(), TensorShape{3, 3});
+    std::copy_n(dx.begin(), dx.size(), x_data.flat<float>().data());
+    LOG(INFO) << x_data.DebugString();
+
+    Tensor y_data(DataTypeToEnum<float>::v(), TensorShape{3, 1});
+    copy_n(dy.begin(), dy.size(), y_data.flat<float>().data());
+
+    for(auto i = 0; i < 1000; i++) {
+        TF_CHECK_OK(session.Run({{x, x_data}, {y, y_data}}, {loss}, &outputs));
+        std::cout << "Loss: " << outputs[0].scalar<float>() << std::endl;
+        TF_CHECK_OK(session.Run({{x, x_data}, {y, y_data}}, {apply_w1, apply_b1}, nullptr));
+    }
     return 0;
 
 }
